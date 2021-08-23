8792d0c0a82cb3bd49086a530602e9213f8980c6
JahazielHernandezHoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2021-08-22 20:03:49 -0500

    Predicción v3

---
+++ Predicción v3.py
@@ -0,0 +1,22 @@
+from google.colab import drive
+drive.mount('/content/drive/')
+
+longitud, altura = 150, 150
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+cnn = load_model(modelo)
+cnn.load_weights(pesos_modelo)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    print("pred: mano_izquierda")
+  elif answer == 1:
+    print("pred: mano_derecha")
+
+  return answer
